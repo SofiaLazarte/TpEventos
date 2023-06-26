990348df44fd11bd4aca2023e27866cf19c3c74b
Sofia Lazarte <sofi.lazarte.sl@gmail.com>
2023-06-26 17:56:46 -0300

    punto tres

--- eventos.js
+++ eventos.js
@@ -16,8 +16,8 @@ function contador()
 boton.addEventListener("click", contador); //añadimos el addeEventListener como funcion, este toma como parametro el evento y la funcion o*/
 
 
-// punto uno ------------------------------------------------
-
+// PUNTO UNO -----------------------------------------------------------------------------------------------------------------------------
+/*
 const formulario = document.querySelector("#formulario");
 const boton = document.querySelector("button");
 var value;
@@ -51,3 +51,229 @@ function numeroAleatorio()
 }
 
 boton.addEventListener("click",numeroAleatorio);
+*/
+
+// PUNTO DOS -------------------------------------------------------------------------------------------------------------------------------
+
+class Persona
+{
+    constructor(nombre, edad, dni, sexo, peso, altura, nacimiento)
+    {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.sexo = sexo;
+        this.peso = peso;
+        this.altura = altura;
+        this.nacimiento = nacimiento;
+
+    }
+
+    mostrarGeneracion()
+    {
+        if (this.nacimiento <= 1948)
+        {
+            alert(`${this.nombre} pertenece a la generacion Silent la cual tiene como razgo caracteristico la austeridad`);
+        }
+
+        else if (this.nacimiento >= 1949 || this.nacimiento <= 1968) 
+        {
+            alert(`${this.nombre} pertenece a la generacion Baby Boom la cual tiene como razgo caracteristico la ambicion`);
+        }
+
+        else if (this.nacimiento >= 1969 || this.nacimiento <= 1980) 
+        {
+            alert(`${this.nombre} pertenece a la generacion X la cual tiene como razgo caracteristico la obsesion por el exito`);
+        }
+
+        else if (this.nacimiento >= 1981 || this.nacimiento <= 1993) 
+        {
+            alert(`${this.nombre} pertenece a la generacion Y o Millennials la cual tiene como razgo caracteristico la frustracion`);
+        }
+
+        else if (this.nacimiento >= 1994 || this.nacimiento <= 2010) 
+        {
+            alert(`${this.nombre} pertenece a la generacion Z la cual tiene como razgo caracteristico la irreverencia`);
+        }
+
+        else 
+        {
+            console.log(`${this.nombre} pertenece a la generacion desconocida`);
+        }
+
+    }
+
+    esMayordeEdad()
+    {
+        if (this.edad >= 18) 
+        {
+            console.log(`${this.nombre} es mayor de edad`);
+        } 
+        else 
+        {
+            console.log(`${this.nombre} es menor de edad`);
+        }
+
+    }
+
+    MostrarDatos()
+    {
+        alert(`${this.nombre} ${this.sexo} de ${this.edad} años, con DNI ${this.dni} y fecha de nacimiento el dia ${this.nacimiento}. Tiene una altura de ${this.altura} cm y un peso de ${this.peso} kg. `);
+    }
+
+    generarDni()
+    {
+        this.dni = (Math.round(Math.random() * (100000000 - 10000000 ) + 10000000));
+        console.log(this.dni)
+    }
+}
+
+var personaUno = new Persona ("Segundo", 25, 48522475, "masculino", 90, 198, 1997);
+
+
+
+/*const formulario = document.getElementById("formulario");
+const botonGeneracion = document.getElementById("verGeneracion");
+const botonEdad = document.getElementById("verEdad");
+var personaNueva;
+
+function crearPersona(event)
+{
+    event.preventDefault();//prevenir el navegador hago cosas con el evento por defecto
+
+    var nombreNuevo = event.target.name.value;
+    var edadNuevo = parseFloat(event.target.number.value);
+    var dniNuevo = parseFloat(event.target.dni.value);
+    var sexoNuevo = event.target.sexo.value;
+    var alturaNuevo = event.target.altura.value;
+    var pesoNuevo = event.target.peso.value;
+    var anioNuevo = parseInt(event.target.anio.value);
+    
+    personaNueva = new Persona(nombreNuevo, edadNuevo, dniNuevo, sexoNuevo, pesoNuevo, alturaNuevo, anioNuevo);
+
+    console.log(personaNueva);
+
+}
+
+
+
+function esMayorDeEdad(event) 
+{
+    event.preventDefault();
+    var nombreNuevo = event.target.name.value;
+    var edadNuevo = parseFloat(event.target.number.value);
+    var dniNuevo = parseFloat(event.target.dni.value);
+    var sexoNuevo = event.target.sexo.value;
+    var alturaNuevo = event.target.altura.value;
+    var pesoNuevo = event.target.peso.value;
+    var anioNuevo = parseInt(event.target.anio.value);
+    
+    personaNueva = new Persona(nombreNuevo, edadNuevo, dniNuevo, sexoNuevo, pesoNuevo, alturaNuevo, anioNuevo);
+
+    personaNueva.esMayordeEdad();
+
+}
+
+function mostrarGeneracion(event) 
+{
+    event.preventDefault();
+    var nombreNuevo = event.target.name.value;
+    var edadNuevo = parseFloat(event.target.number.value);
+    var dniNuevo = parseFloat(event.target.dni.value);
+    var sexoNuevo = event.target.sexo.value;
+    var alturaNuevo = event.target.altura.value;
+    var pesoNuevo = event.target.peso.value;
+    var anioNuevo = parseInt(event.target.anio.value);
+    
+    personaNueva = new Persona(nombreNuevo, edadNuevo, dniNuevo, sexoNuevo, pesoNuevo, alturaNuevo, anioNuevo);
+
+    personaNueva.mostrarGeneracion();
+
+}
+
+formulario.addEventListener('submit', crearPersona);
+botonGeneracion.addEventListener('click', mostrarGeneracion);
+botonEdad.addEventListener('click', esMayorDeEdad);*/
+
+/*const formulario = document.querySelector("#formulario");
+const botonGeneracion = document.querySelector("#verGeneracion");
+const botonEdad = document.querySelector("#verEdad");
+var personaNueva = new Persona();
+
+function valoresFormulario(event)
+{
+    event.preventDefault();//prevenir el navegador hago cosas con el evento por defecto
+
+    var nombreNuevo = event.target.name.value;
+    var edadNuevo = event.target.number.value;
+    var dniNuevo = event.target.dni.value;
+    var sexoNuevo = event.target.sexo.value;
+    var alturaNuevo = event.target.altura.value;
+    var pesoNuevo = event.target.peso.value;
+    var anioNuevo = event.target.anio.value;
+
+    //personaNueva = new Persona(nombreNuevo, edadNuevo, dniNuevo, sexoNuevo, pesoNuevo, alturaNuevo, anioNuevo);
+    personaNueva.nombre = nombreNuevo;
+    personaNueva.nacimiento = anioNuevo;
+
+    console.log(personaNueva);
+}
+
+formulario.addEventListener("submit", valoresFormulario);
+
+function verGeneracion() 
+{
+    console.log(personaNueva);
+    personaNueva.mostrarGeneracion();
+}
+
+botonGeneracion.addEventListener("click", verGeneracion);
+*/
+
+//PUNTO TRES ----------------------------------------------------------------------------------------------------------------------------
+
+const input = document.querySelector("#input");
+const btnAdd = document.querySelector("#btn-add");
+const ul = document.querySelector("ul");
+
+function anadirLista(e) 
+{
+    e.preventDefault();
+    var text = input.value; //aqui guardo el valor del input en la variable text
+
+    if (text !== "") 
+    {
+        var li = document.createElement("li");//creo un nuevo elemento htlm fr yipo <li> y la almaceno en la variale li
+        var p = document.createElement("p");//idem anterior
+    
+        p.textContent = text;//con esto el texto que almacene en la variable "text" lo asigno al elemento p, osea que la nueva tarea ingresada sera el contenido del parrafo
+    
+        li.appendChild(p);//con esto agrego el elemento parrafo como hijo de el elemento lista "li"
+        li.appendChild(crearBtnDelete());
+    
+        ul.appendChild(li);//  con este agrego a los li como hijos de ul (lista des)
+    
+        input.value = "";//borrar el impunt cuando pulsemos añadir 
+    }
+
+}
+
+btnAdd.addEventListener("click", anadirLista);
+
+function borrar(e) 
+{
+    var item = e.target.parentElement;
+    ul.removeChild(item);
+}
+
+function crearBtnDelete() 
+{
+    var btnDelete = document.createElement("button");
+    btnDelete.textContent = "x";
+    btnDelete.addEventListener("click", borrar);
+
+    return btnDelete;   
+}
+
+
+
